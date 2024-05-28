710d3a0c9be046fe1b66c376f0a2aafc4d18bb0c
estroganova <e.stroganova@vehikl.com>
2024-05-28 09:31:50 -0400

    wip: add setting and displaying userNotes

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     const [rawPayload, setRawPayload] = useState({});
     const [sessionDates, setSessionDates] = useState([]);
     const [sessionInfo, setSessionInfo] = useState([]);
+    const [userNotes, setUserNotes] = useState('');
 
     useEffect(() => {
         axios.get('https://growth.vehikl.com/growth_sessions/week?date=2024-02-26')
@@ -32,12 +33,15 @@ function App() {
             <div key={sessionArray[0].id} className="Session">
                 <h2>Title: {sessionArray[0].title}</h2>
                 <p>Description: {sessionArray[0].topic}</p>
-                <textarea placeholder="Add notes..."></textarea>
+                <textarea onChange={handleUserNotes} id="userNotes" value={userNotes}
+                          placeholder="Add notes..."></textarea>
             </div>
         ));
     }
 
-    console.log(sessionInfo);
+    const handleUserNotes = event => setUserNotes(event.target.value);
+
+    console.log(userNotes);
 
     return (
         <div className="App">
